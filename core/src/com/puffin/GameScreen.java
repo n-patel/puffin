c7b688bf18f003ef592cd532d760496bf7807058
Lear <dulear314@gmail.com>
2015-10-10 13:46:47 -0700

    Framework set up Constants: Set the constant values for the game. GameScreen: Defined render GameStage: Create a world object Worldutils: used to create bodies

---
+++ core/src/com/puffin/GameScreen.java
@@ -0,0 +1,57 @@
+package com.puffin;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.puffin.GameStage;
+
+
+public class GameScreen implements Screen {
+    private GameStage stage;
+
+    public GameScreen() {
+        stage = new GameStage();
+    }
+
+    @Override
+    public void render(float delta) {
+        //Clear the screen
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        //Update the stage
+        stage.draw();
+        stage.act(delta);
+    }
+
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+}
