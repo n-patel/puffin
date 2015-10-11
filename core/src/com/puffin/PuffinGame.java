b8ca45020c57123339b4bd568bb79d21b933dc3e
Nikhil Patel <n.patel@berkeley.edu>
2015-10-11 11:59:12 -0700

    Add main menu/splash screen

--- core/src/com/puffin/PuffinGame.java
+++ core/src/com/puffin/PuffinGame.java
@@ -1,8 +1,11 @@
 package com.puffin;
 import com.badlogic.gdx.Game;
+import com.puffin.view.MainMenu;
+
 public class PuffinGame extends Game {
 	@Override
 	public void create() {
-		setScreen(new GameScreen());
+		//setScreen(new GameScreen());
+		setScreen(new MainMenu(this));
 	}
 }
---
+++ core/src/com/puffin/view/MainMenu.java
@@ -0,0 +1,66 @@
+package com.puffin.view;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.puffin.GameScreen;
+
+/**
+ * Created by Nikhil on 10/11/15.
+ */
+public class MainMenu implements Screen {
+
+    private Game game;
+    private SpriteBatch spriteBatch;
+    private Texture splashScreen;
+
+    public MainMenu(Game game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+        spriteBatch = new SpriteBatch();
+        splashScreen = new Texture("background1.png");
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        spriteBatch.begin();
+        spriteBatch.draw(splashScreen, 0, 0);
+        spriteBatch.end();
+
+        if (Gdx.input.justTouched()) {
+            game.setScreen(new GameScreen());
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
