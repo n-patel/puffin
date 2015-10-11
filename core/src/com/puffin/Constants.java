510614139441ad581f0c740af8c3622e15fd9a89
Nikhil Patel <n.patel@berkeley.edu>
2015-10-11 00:10:10 -0700

    Adjust projectile spawnpoint and speed

--- core/src/com/puffin/Constants.java
+++ core/src/com/puffin/Constants.java
@@ -16,7 +16,7 @@ public class Constants {
     public static final float GROUND_DENSITY = 0.0f;
     public static final Vector2 GROUND_SPEED = new Vector2(-7f, 0f);
 
-    public static final float RUNNER_X             = 2;
+    public static final float RUNNER_X             = 2.5f;
     public static final float RUNNER_Y             = GROUND_Y + GROUND_HEIGHT;
     public static final float RUNNER_WIDTH         = 1.2f;
     public static final float RUNNER_HEIGHT        = 2.1f;
@@ -24,12 +24,11 @@ public class Constants {
     public static final float RUNNER_GRAVITY_SCALE = 3.0f;
     public static final Vector2 RUNNER_JUMPING_LINEAR_IMPULSE = new Vector2(0, 40 * RUNNER_DENSITY);
 
-    public static final float PROJECTILE_SPEED  = 2.0f;      //sets the projectile speed constant
+    public static final float PROJECTILE_SPEED  = 5.0f;      //sets the projectile speed constant
     public static final float PROJECTILE_WIDTH  = 0.6f;
     public static final float PROJECTILE_HEIGHT = 0.6f;
+    public static final float FIRE_DELAY        = 0.5f;
 
     public static final int VIEWPORT_WIDTH  = 20;
     public static final int VIEWPORT_HEIGHT = 13;
-
-    public static final int FIRE_DELAY = 10;
 }
