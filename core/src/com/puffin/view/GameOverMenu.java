af974070353659b8232e0b4f755297ae3f1b6dee
Jessie Yang <j.pure.serendipity@gmail.com>
2015-10-11 17:38:00 -0700

    Create GameOverMenu and add art for game over screen

---
+++ core/src/com/puffin/view/GameOverMenu.java
@@ -0,0 +1,68 @@
+package com.puffin.view;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.puffin.GameScreen;
+
+/**
+ * Created by Jessie on 10/11/15.
+ */
+public class GameOverMenu implements Screen {
+
+    private Game game;
+    private SpriteBatch spriteBatch;
+    private Sprite splashScreen;
+
+    public GameOverMenu(Game game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+        spriteBatch = new SpriteBatch();
+        splashScreen = new Sprite(new Texture("end-background.png"));
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        spriteBatch.begin();
+        splashScreen.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        splashScreen.draw(spriteBatch);;
+        spriteBatch.end();
+
+        if (Gdx.input.justTouched()) {
+            game.setScreen(new GameScreen());
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
